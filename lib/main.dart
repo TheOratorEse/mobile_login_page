962bf870df07334696068221068ba3a6305e6b4a
Orator Ese <oratoreseiwi@gmail.com>
2024-12-16 20:31:14 +0100

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -11,14 +11,48 @@ class MyHome extends StatelessWidget {
         title: 'My Flutter App',
         home: Scaffold(
           appBar: AppBar(
-            backgroundColor: Colors.deepPurpleAccent,
+            backgroundColor: Colors.deepPurple,
             //title: Text('My App'),
+          ),
+          drawer: Drawer(
+            backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+            child: ListView(children: [
+              DrawerHeader(
+                  decoration: BoxDecoration(
+                      // color: const Color.fromARGB(255, 104, 58, 183),
+                      image: DecorationImage(
+                          image: NetworkImage(
+                              'https://i.ibb.co/mD0Tr1Q/bg-image.jpg'),
+                          fit: BoxFit.fill)),
+                  child: Text(
+                    'Menu',
+                    style: TextStyle(fontSize: 35, color: Colors.white),
+                    textAlign: TextAlign.left,
+                  )),
+              ListTile(
+                title: Text(
+                  'My Profile',
+                  style: TextStyle(color: Colors.black, fontSize: 20),
+                ),
+                leading: Icon(
+                  Icons.person_3_outlined,
+                  color: Colors.deepPurple,
+                  size: 25,
+                ),
+              )
+            ]),
           ),
           body: Column(
             children: [
               HomeScreen(),
             ],
           ),
+          persistentFooterButtons: [
+            Text.rich(
+              TextSpan(
+                  text: 'By continuing, uou agree to our terms of service'),
+            ),
+          ],
         ));
   }
 }
@@ -34,38 +68,57 @@ class HomeScreen extends StatelessWidget {
         children: <Widget>[
           Container(
             alignment: Alignment.center,
-            height: MediaQuery.sizeOf(context).height *0.2,
-           // width: 400,
+            height: MediaQuery.sizeOf(context).height * 0.2,
+            // width: 400,
             margin: EdgeInsets.only(top: 10, bottom: 30),
-            decoration: BoxDecoration( image: DecorationImage(image: NetworkImage('https://i.ibb.co/mD0Tr1Q/bg-image.jpg',), fit: BoxFit.fill), shape: BoxShape.circle),
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: NetworkImage(
+                      'https://i.ibb.co/mD0Tr1Q/bg-image.jpg',
+                    ),
+                    fit: BoxFit.fill),
+                shape: BoxShape.circle),
             child: Text(
               'Welcome back',
-              style: TextStyle(color: Color.fromARGB(255, 255, 255, 255), fontSize: 20),
+              style: TextStyle(
+                  color: Color.fromARGB(255, 255, 255, 255), fontSize: 20),
             ),
           ),
           Form(
-            child: Column(
-              children: <Widget>[
-                TextFormField(
-                  decoration: InputDecoration(
-                      alignLabelWithHint: true,
-                      icon: Icon(Icons.person),
-                      label: Text('Username'),
-                      hintText: 'Enter your username here'),
-                ),
-                TextFormField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                      icon: Icon(Icons.password),
-                      label: Text('Password'),
-                      hintText: 'Enter your password'),
-                ),
-                SizedBox(height: 10),
-                FilledButton(
-                  onPressed: () {},
-                  child: Text('Login'),
-                )
-              ],
+            child: Center(
+              child: Column(
+                spacing: 25,
+                children: <Widget>[
+                  TextFormField(
+                    decoration: InputDecoration(
+                        //icon: Icon(Icons.person),
+                        prefixIcon: Icon(Icons.person_2_outlined),
+                        label: Text('Username'),
+                        hintText: 'Enter your username here',
+                        contentPadding: EdgeInsets.symmetric(
+                            vertical: 16.0, horizontal: 16.0),
+                        border: OutlineInputBorder()),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        //  icon: Icon(Icons.password),
+                        prefixIcon: Icon(Icons.password_outlined),
+                        label: Text('Password'),
+                        hintText: 'Enter your password',
+                        contentPadding: EdgeInsets.symmetric(
+                            vertical: 16.0, horizontal: 16.0),
+                        border: OutlineInputBorder()),
+                  ),
+                  FilledButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Login',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ],
